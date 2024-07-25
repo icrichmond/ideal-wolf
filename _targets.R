906854cae658c14e96378bc5f47f9affa4432f72
icrichmond <bellarichmond@proton.me>
2024-07-25 14:45:08 -0400

    start modelling vv

--- _targets.R
+++ _targets.R
@@ -27,8 +27,60 @@ c(
     summ_data,
     'data/2021-09-19-Bloc-DielModel.csv',
     read.csv(!!.x)
-  )
+  ),
   
+  tar_file_read(
+    vv_data,
+    'data/vander-vennen/VanderVennen_etal_WolfMooseKillRates.csv',
+    read.csv(!!.x) |>
+      rename(TimeBin = Time.Bin,
+             KillRate = Kill.Rate,
+             MooseSpeed = Moose.Speed,
+             WolfSpeed = Wolf.Speed,
+             CrepuscularLight = Crepuscular.Light,
+             TotalLight = Total.Light)
+  ),
+  
+  # rate ~ effective_speed * distance/detectability * density
+  # effective_speed = sqrt(predator_speed^2 + prey_speed^2)
+  
+  zar_brms(
+    vv_model,
+    # VV log-transformed so equation would be additive + counted light as detectability
+    formula = bf(KillRate ~ effectspeed + CrepuscularLight + TotalLight,
+                 effectspeed ~ sqrt(MooseSpeed^2 + WolfSpeed^2),
+                 CrepuscularLight + TotalLight ~ 1,
+                 nl=TRUE),
+    prior = c(
+      prior(exponential(1), class = 'sigma'),
+      prior(normal(0.5, 1), lb = 0, nlpar = "effectspeed"),
+      prior(normal(0.5, 1), lb = 0, nlpar = "CrepuscularLight"),
+      prior(normal(0.5, 1), lb = 0, nlpar = "TotalLight")),
+    family = gaussian(),
+    data = vv_data,
+    chains = 4,
+    iter = 2000,
+    cores = 4,
+    backend = 'cmdstanr'
+  ),
+  
+  tar_target(
+    model_prior_list,
+    list(vv_model_brms_sample_prior) %>% 
+      setNames(., 'VanderVennen_prior')
+  ),
+  
+  tar_target(
+    model_list,
+    list(vv_model_brms_sample) %>% 
+      setNames(., 'VanderVennen')
+  ),
+  
+  tar_render(
+    prior_checks,
+    path = 'R/prior_predictive.qmd',
+    working_directory = NULL
+  )
   
   
 )
